ecd7a3bb642afc51b8ae59ec09ae49f7f6307440
yotamfr <yotamfr@hotmail.com>
2017-10-27 16:02:02 +0300

    Fixed a bug where model was saved under very long filename.

--- src/python/kmer_embedding.py
+++ src/python/kmer_embedding.py
@@ -10,10 +10,12 @@ parser.add_argument("-n", "--kmer", type=int, required=True,
                     help="Give the length of the kmer.")
 parser.add_argument("-c", "--context", type=int, required=True,
                     help="Give the length of the context window.")
+parser.add_argument("-d", "--emb_dim", type=int, required=True,
+                    help="Give the dimension of the embedding vector.")
 parser.add_argument("--mongo_url", type=str, default='mongodb://localhost:27017/',
                     help="Supply the URL of MongoDB")
-parser.add_argument("-i", "--collection", type=str, choices=['uniprot', 'sprot'],
-                    default="sprot", help="Give the collection name.")
+parser.add_argument("-s", "--source", type=str, choices=['uniprot', 'sprot'],
+                    default="sprot", help="Give source name.")
 parser.add_argument("-o", "--outputdir", type=str,  required=False,
                     default="models", help="Specify the output directory")
 parser.add_argument("-t", "--num_threads", type=int,  required=False,
@@ -22,7 +24,7 @@ args = parser.parse_args()
 
 client = MongoClient(args.mongo_url)
 db_name = 'prot2vec'
-collection = client[db_name][args.collection]
+collection = client[db_name][args.source]
 
 ckptpath = args.outputdir
 if not os.path.exists(ckptpath):
@@ -51,13 +53,14 @@ class KmerSentences(object):
 
 class Word2VecWrapper(object):
 
-    def __init__(self, ngram_size, win_size, dim_size=100, min_count=2):
+    def __init__(self, ngram_size, win_size, dim_size, min_count=2):
 
+        s = args.source
         t = args.num_threads
         k, c, d, mc = ngram_size, win_size, dim_size, min_count
 
-        unique_str = "%s:%s-mer:dim=%s:c=%s:mc=%s" % (k, collection, d, c, mc)
-        model_filename = "%s/%s" % (ckptpath, unique_str)
+        unique_str = "%s_%s-mer_dim=%s_c=%s_mc=%s" % (s, k, d, c, mc)
+        model_filename = "%s/%s.emb" % (ckptpath, unique_str)
         if os.path.exists(model_filename):
             self._model = Word2Vec.load(model_filename)
         else:
@@ -83,4 +86,4 @@ class Word2VecWrapper(object):
 
 
 if __name__=="__main__":
-    Word2VecWrapper(args.kmer, args.context)
+    Word2VecWrapper(args.kmer, args.context, args.emb_dim)
