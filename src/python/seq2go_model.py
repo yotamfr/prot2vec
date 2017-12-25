61ea67d332f79d44345f38e3a259388be0a6813d
yotamfr <yotamfr@hotmail.com>
2017-12-25 17:08:51 +0200

    small bug-fixes

--- src/python/seq2go_model.py
+++ src/python/seq2go_model.py
@@ -227,7 +227,7 @@ class LuongAttnDecoderRNN(nn.Module):
         batch_size = input_seq.size(0)
         embedded = self.embedding(input_seq)
         embedded = self.embedding_dropout(embedded)
-        embedded = embedded.view(1, batch_size, self.embedding_size)  # S=1 x B x N
+        embedded = embedded.view(1, batch_size, -1)  # S=1 x B x N
 
         # Get current hidden state from input word and last hidden state
         rnn_output, hidden = self.gru(embedded, last_hidden)
