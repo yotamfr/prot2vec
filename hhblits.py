dbf630c18037a970467328fe20f5f2c64171433d
yotamfr <yotamfr@hotmail.com>
2018-01-13 16:28:31 +0200

    Fine-tuning to the parameters of hhblits.

--- hhblits.py
+++ hhblits.py
@@ -137,12 +137,12 @@ def _run_hhblits_batched(sequences, cleanup=False):
         handle, _ = child.communicate()
         assert child.returncode == 0
 
-        hhblits_cmd = "hhblits_omp -i $file -d ../dbs/%s/%s -oa3m $name.a3m -n 2 -maxfilt %d -mact 0.9 -cpu %d" % \
+        hhblits_cmd = "hhblits -i $file -d ../dbs/%s/%s -oa3m $name.a3m -n 2 -maxfilt %d -mact 0.9 -cpu %d" % \
                       (uniprot20name, uniprot20name, max_filter, num_cpu)
         cline = "%s/multithread.pl \'*.seq\' \'%s\'" % (prefix_hhsuite, hhblits_cmd)
         child = subprocess.Popen(cline,
-                                 stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE,
+                                 # stdout=subprocess.PIPE,
+                                 # stderr=subprocess.PIPE,
                                  universal_newlines=True,
                                  shell=(sys.platform != "win32"))
 
