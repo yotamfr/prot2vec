4e5ac86dfa85441ffbb8375e733e05b7dfd282b1
yotamfr <yotamfr@hotmail.com>
2018-02-12 15:28:28 +0200

    experimental- feed in just the sequence data

--- src/python/benchmark-1.py
+++ src/python/benchmark-1.py
@@ -104,10 +104,11 @@ def data_generator(seq2pssm, seq2go, classes):
             y[classes.index(go)] = 1
 
         seq, pssm, msa = seq2pssm[k]
-        x1 = [[AA.aa2onehot[aa] + [pssm[i][AA.index2aa[k]] for k in range(20)]] for i, aa in enumerate(seq)]
+        x0 = [AA.aa2index[aa] for aa in seq]
+        x1 = [[AA.aa2onehot[aa], [pssm[i][AA.index2aa[k]] for k in range(20)]] for i, aa in enumerate(seq)]
         x2 = msa
 
-        yield k, x1, x2, y
+        yield k, x0, x1, x2, y
 
 
 def step_decay(epoch):
@@ -119,23 +120,51 @@ def step_decay(epoch):
    return lrate
 
 
-def Motifs(inpt):
-    motif01 = Conv2D(192, (1, 40), padding='valid', activation='relu')(inpt)
-    motif03 = Conv2D(64, (3, 1), padding='same', activation='relu')(motif01)
-    motif09 = Conv2D(64, (9, 1), padding='same', activation='relu')(motif01)
-    motif18 = Conv2D(64, (18, 1), padding='same', activation='relu')(motif01)
-    motif36 = Conv2D(64, (36, 1), padding='same', activation='relu')(motif01)
+# def Motifs(inpt):
+#     motif01 = Conv2D(192, (1, 40), padding='valid', activation='relu')(inpt)
+#     motif03 = Conv2D(64, (3, 1), padding='same', activation='relu')(motif01)
+#     motif09 = Conv2D(64, (9, 1), padding='same', activation='relu')(motif01)
+#     motif18 = Conv2D(64, (18, 1), padding='same', activation='relu')(motif01)
+#     motif36 = Conv2D(64, (36, 1), padding='same', activation='relu')(motif01)
+#
+#     return Concatenate(axis=1)([motif03, motif09, motif18, motif36])
+#
+#
+# def Features(inpt):
+#     out = inpt
+#     out = Conv2D(64, (3, 1), activation='relu', padding='same')(out)
+#     out = Conv2D(64, (3, 1), activation='relu', padding='same')(out)
+#     out = MaxPooling2D((2, 1))(out)
+#     out = Conv2D(128, (3, 1), activation='relu', padding='same')(out)
+#     out = Conv2D(128, (3, 1), activation='relu', padding='same')(out)
+#
+#     out = GlobalMaxPooling2D(data_format='channels_first')(out)
+#     return out
 
-    return Concatenate(axis=1)([motif03, motif09, motif18, motif36])
+def Motifs(inpt):
+
+    embed = Embedding(input_dim=25, output_dim=20)(inpt)
+    embed = Reshape((1, -1, 20))(embed)
+    motif01 = Conv2D(1024, (1, 20), data_format='channels_first', padding='valid', activation='relu')(embed)
+    motif03 = Conv2D(768, (3, 1), data_format='channels_first', padding='same', activation='relu')(motif01)
+    motif03 = Reshape((1, -1, 768))(motif03)
+    motif09 = Conv2D(256, (9, 1), data_format='channels_first', padding='same', activation='relu')(motif01)
+    motif09 = Reshape((1, -1, 256))(motif09)
+    motif18 = Conv2D(128, (18, 1), data_format='channels_first',  padding='same', activation='relu')(motif01)
+    motif18 = Reshape((1, -1, 128))(motif18)
+    motif36 = Conv2D(64, (36, 1), data_format='channels_first', padding='same', activation='relu')(motif01)
+    motif36 = Reshape((1, -1, 64))(motif36)
+    return Concatenate(axis=3)([motif03, motif09, motif18, motif36])
 
 
 def Features(inpt):
     out = inpt
-    out = Conv2D(64, (3, 1), activation='relu', padding='same')(out)
-    out = Conv2D(64, (3, 1), activation='relu', padding='same')(out)
-    out = MaxPooling2D((2, 1))(out)
-    out = Conv2D(128, (3, 1), activation='relu', padding='same')(out)
-    out = Conv2D(128, (3, 1), activation='relu', padding='same')(out)
+    out = Conv2D(512, (3, 1216), data_format='channels_first', activation='relu', padding='valid')(out)
+    # out = MaxPooling2D((2, 1))(out)
+    out = Conv2D(512, (7, 1), data_format='channels_first', activation='relu', padding='valid')(out)
+    # out = MaxPooling2D((2, 1))(out)
+    # out = Conv2D(128, (3, 1), activation='relu', padding='same')(out)
+    # out = Conv2D(128, (3, 1), activation='relu', padding='same')(out)
 
     out = GlobalMaxPooling2D(data_format='channels_first')(out)
     # return Flatten()(out)
@@ -145,7 +174,8 @@ def Features(inpt):
 def Classifier(inpt, hidden_size, classes):
     x = inpt
     # We stack a deep densely-connected network on top
-    x = Dense(hidden_size * 2, activation='relu')(x)
+    # x = Dropout(0.1)(inpt)
+    # x = Dense(hidden_size * 2, activation='relu')(x)
     x = Dense(hidden_size, activation='relu')(x)
 
     # And finally we add the main logistic regression layer
@@ -153,9 +183,10 @@ def Classifier(inpt, hidden_size, classes):
 
 
 def ModelCNN(classes):
-    inp = Input(shape=(1, None, 40))
-    out = Classifier(Features(Motifs(inp)), 64, classes)
-    model = Model(inputs=[inp], outputs=[out])
+    inp1 = Input(shape=(None,))
+    # inp2 = Input(shape=(2, None, 20))
+    out = Classifier(Features(Motifs(inp1)), 128, classes)
+    model = Model(inputs=[inp1], outputs=[out])
     # sgd = optimizers.SGD(lr=LR, decay=1e-6, momentum=0.9, nesterov=True)
     sgd = optimizers.SGD(lr=LR, momentum=0.9, nesterov=True)
     model.compile(loss='hinge', optimizer=sgd)
@@ -163,22 +194,25 @@ def ModelCNN(classes):
     return model
 
 
-def get_xy(gen, normalize=False):
-    xByShapes, yByShapes = dict(), dict()
-    for _, x, _, y in gen:
-        x, y = np.array(x).reshape(1, len(x), 40), zeroone2oneminusone(y)
+def get_sxy(gen, normalize=False):
+    sByShapes, xByShapes, yByShapes = dict(), dict(), dict()
+    for _, s, x, _, y in gen:
+        x, y = np.array(x).reshape(2, len(s), 20), zeroone2oneminusone(y)
         if normalize: x = np.divide(np.add(x, -np.mean(x)), np.std(x))
         if x.shape in xByShapes:
+            sByShapes[x.shape].append(s)
             xByShapes[x.shape].append(x)
             yByShapes[x.shape].append(y)
         else:
-            xByShapes[x.shape] = [x]  # initially a list, because we're going to append items
+            sByShapes[x.shape] = [s]
+            xByShapes[x.shape] = [x]
             yByShapes[x.shape] = [y]
 
     xByShapes = {k: np.array(v) for k, v in xByShapes.items()}
     yByShapes = {k: np.array(v) for k, v in yByShapes.items()}
+    sByShapes = {k: np.array(v) for k, v in sByShapes.items()}
 
-    return xByShapes, yByShapes
+    return sByShapes, xByShapes, yByShapes
 
 
 def add_arguments(parser):
@@ -204,7 +238,7 @@ def train(model, X, Y, epoch, num_epochs, history=LossHistory(), lrate=LearningR
     m = sum(map(lambda k: len(Y[k]), Y.keys()))
     pbar = tqdm(total=m)
     for x_shp, y_shp in zip(X.keys(), Y.keys()):
-        model.fit(x=trn_X[x_shp], y=trn_Y[y_shp],
+        model.fit(x=X[x_shp], y=Y[y_shp],
                   batch_size=8, epochs=num_epochs,
                   verbose=0,
                   validation_data=None,
@@ -262,8 +296,8 @@ if __name__ == "__main__":
 
     classes = onto.get_level(1)
 
-    trn_X, trn_Y = get_xy(data_generator(trn_seq2pssm, trn_seq2go, classes))
-    tst_X, tst_Y = get_xy(data_generator(tst_seq2pssm, tst_seq2go, classes))
+    trn_seq, trn_X, trn_Y = get_sxy(data_generator(trn_seq2pssm, trn_seq2go, classes))
+    tst_seq, tst_X, tst_Y = get_sxy(data_generator(tst_seq2pssm, tst_seq2go, classes))
 
     model = ModelCNN(classes)
     print(model.summary())
@@ -283,8 +317,8 @@ if __name__ == "__main__":
 
     sess = tf.Session()
     for epoch in range(args.num_epochs):
-        train(model, trn_X, trn_Y, epoch, args.num_epochs)
-        _, _, loss, f_max = evaluate(model, tst_X, tst_Y, classes)
+        train(model, trn_seq, trn_Y, epoch, args.num_epochs)
+        _, _, loss, f_max = evaluate(model, tst_seq, tst_Y, classes)
         print("[Epoch %d] (Validation Loss: %.5f, F_max: %.3f)" % (epoch + 1, loss, f_max))
         # tst_shapes = list(zip(tst_X.keys(), tst_Y.keys()))
         # for trnXshape, trnYshape in zip(trn_X.keys(), trn_Y.keys()):
