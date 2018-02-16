d66e3cf2ed1219ca20eb89484ed1cb80d5c3737c
yotamfr <yotamfr@hotmail.com>
2018-02-16 18:28:38 +0200

    benchmark-0.py changed everything to generators.

--- src/python/benchmark-0.py
+++ src/python/benchmark-0.py
@@ -1,6 +1,6 @@
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 from src.python.consts import *
 
@@ -9,8 +9,6 @@ from src.python.preprocess import *
 from src.python.geneontology import *
 
 from src.python.baselines import *
-
-from sklearn.metrics import hinge_loss
 
 from pymongo import MongoClient
 
@@ -40,76 +38,6 @@ import argparse
 
 
 LR = 0.032
-
-
-def _get_labeled_data(db, query, limit, pssm=True):
-
-    c = limit if limit else db.goa_uniprot.count(query)
-    s = db.goa_uniprot.find(query)
-    if limit: s = s.limit(limit)
-
-    seqid2goid, _ = GoAnnotationCollectionLoader(s, c, ASPECT).load()
-
-    query = {"_id": {"$in": unique(list(seqid2goid.keys())).tolist()}, "pssm": {"$exists": True}}
-
-    if pssm:
-        num_seq = db.pssm.count(query)
-        src_seq = db.pssm.find(query)
-        seqid2seq = PssmCollectionLoader(src_seq, num_seq).load()
-    else:
-        num_seq = db.uniprot.count(query)
-        src_seq = db.uniprot.find(query)
-        seqid2seq = UniprotCollectionLoader(src_seq, num_seq).load()
-
-    seqid2goid = {k: v for k, v in seqid2goid.items() if len(v) > 1 or 'GO:0005515' not in v}
-    seqid2seq = {k: v for k, v in seqid2seq.items() if k in seqid2goid and len(v[0]) > 30}
-    seqid2goid = {k: v for k, v in seqid2goid.items() if k in seqid2seq}
-
-    return seqid2seq, seqid2goid
-
-
-def load_training_and_validation(db, limit=None):
-    q_train = {'DB': 'UniProtKB',
-               'Evidence': {'$in': exp_codes},
-               'Date':  {"$lte": t0},
-               'Aspect': ASPECT}
-
-    sequences_train, annotations_train = _get_labeled_data(db, q_train, limit)
-
-    q_valid = {'DB': 'UniProtKB',
-               'Evidence': {'$in': exp_codes},
-               'Date':  {"$gt": t0, "$lte": t1},
-               'Aspect': ASPECT}
-
-    sequences_valid, annotations_valid = _get_labeled_data(db, q_valid, limit)
-    forbidden = set(sequences_train.keys())
-    sequences_valid = {k: v for k, v in sequences_valid.items() if k not in forbidden}
-    annotations_valid = {k: v for k, v in annotations_valid.items() if k not in forbidden}
-
-    return sequences_train, annotations_train, sequences_valid, annotations_valid
-
-
-def data_generator(seq2pssm, seq2go, classes):
-    s_cls = set(classes)
-    for i, (k, v) in enumerate(seq2go.items()):
-
-        sys.stdout.write("\r{0:.0f}%".format(100.0 * i / len(seq2go)))
-
-        if k not in seq2pssm:
-            continue
-        y = np.zeros(len(classes))
-        for go in onto.propagate(seq2go[k]):
-            if go not in s_cls:
-                continue
-            y[classes.index(go)] = 1
-
-        seq, pssm, msa = seq2pssm[k]
-        if not pssm: continue
-
-        x1 = [[AA.aa2onehot[aa] + [pssm[i][AA.index2aa[k]] for k in range(20)]] for i, aa in enumerate(seq)]
-        x2 = msa
-
-        yield k, x1, x2, y
 
 
 def step_decay(epoch):
@@ -163,24 +91,6 @@ def ModelCNN(classes):
     return model
 
 
-def get_xy(gen, normalize=False):
-    xByShapes, yByShapes = dict(), dict()
-    for _, x, _, y in gen:
-        x, y = np.array(x).reshape(1, len(x), 40), zeroone2oneminusone(y)
-        if normalize: x = np.divide(np.add(x, -np.mean(x)), np.std(x))
-        if x.shape in xByShapes:
-            xByShapes[x.shape].append(x)
-            yByShapes[x.shape].append(y)
-        else:
-            xByShapes[x.shape] = [x]  # initially a list, because we're going to append items
-            yByShapes[x.shape] = [y]
-
-    xByShapes = {k: np.array(v) for k, v in xByShapes.items()}
-    yByShapes = {k: np.array(v) for k, v in yByShapes.items()}
-
-    return xByShapes, yByShapes
-
-
 def add_arguments(parser):
     parser.add_argument("--mongo_url", type=str, default='mongodb://localhost:27017/',
                         help="Supply the URL of MongoDB")
@@ -200,20 +110,27 @@ class LossHistory(Callback):
         self.losses.append(logs.get('loss'))
 
 
-def train(model, X, Y, epoch, num_epochs, history=LossHistory(), lrate=LearningRateScheduler(step_decay)):
-    m = sum(map(lambda k: len(Y[k]), Y.keys()))
-    pbar = tqdm(total=m)
-    for x_shp, y_shp in zip(X.keys(), Y.keys()):
-        model.fit(x=X[x_shp], y=Y[y_shp],
-                  batch_size=8, epochs=num_epochs,
-                  verbose=0,
-                  validation_data=None,
-                  initial_epoch=epoch,
-                  callbacks=[history, lrate])
-        pbar.set_description("Training Loss:%.5f" % np.mean(history.losses))
-        pbar.update(len(Y[y_shp]))
+def get_training_and_validation_streams(db, classes):
+    q_train = {'DB': 'UniProtKB',
+               'Evidence': {'$in': exp_codes},
+               'Date': {"$lte": t0},
+               'Aspect': ASPECT}
+    seq2go_trn, _ = GoAnnotationCollectionLoader(db.goa_uniprot.find(q_train), db.goa_uniprot.count(q_train),
+                                                 ASPECT).load()
+    query = {"_id": {"$in": unique(list(seq2go_trn.keys())).tolist()}, "pssm": {"$exists": True}}
+    stream_trn = DataStream(db.pssm.find(query), db.pssm.count(query), seq2go_trn, classes)
 
-    pbar.close()
+    q_valid = {'DB': 'UniProtKB',
+               'Evidence': {'$in': exp_codes},
+               'Date': {"$gt": t0, "$lte": t1},
+               'Aspect': ASPECT}
+
+    seq2go_tst, _ = GoAnnotationCollectionLoader(db.goa_uniprot.find(q_valid), db.goa_uniprot.count(q_valid),
+                                                 ASPECT).load()
+    query = {"_id": {"$in": unique(list(seq2go_tst.keys())).tolist()}, "pssm": {"$exists": True}}
+    stream_tst = DataStream(db.pssm.find(query), db.pssm.count(query), seq2go_tst, classes)
+
+    return stream_trn, stream_tst
 
 
 def zeroone2oneminusone(vec):
@@ -224,20 +141,134 @@ def oneminusone2zeroone(vec):
     return np.divide(np.add(np.array(vec), 1), 2)
 
 
-def evaluate(model, X, Y, classes):
-    i, m, n = 0, sum(map(lambda k: len(Y[k]), Y.keys())), len(classes)
+class DataStream(object):
+    def __init__(self, source, count, seq2go, classes):
+
+        self._classes = classes
+        self._count = count
+        self._source = source
+        self._seq2go = seq2go
+
+    def __iter__(self):
+
+        classes = self._classes
+        count = self._count
+        source = self._source
+        seq2go = self._seq2go
+
+        s_cls = set(classes)
+
+        for k, (seq, pssm, msa) in PssmCollectionLoader(source, count):
+            if len(seq) < 30: continue
+            y = np.zeros(len(classes))
+            for go in onto.propagate(seq2go[k]):
+                if go not in s_cls:
+                    continue
+                y[classes.index(go)] = 1
+
+            if not pssm: continue
+
+            x1 = [[AA.aa2onehot[aa] + [pssm[i][AA.index2aa[k]] for k in range(20)]] for i, aa in enumerate(seq)]
+            x2 = msa
+
+            yield k, x1, x2, y
+
+    def __len__(self):
+        return self._count
+
+
+def data_generator(seq2pssm, seq2go, classes, verbose=1):
+    s_cls = set(classes)
+    for i, (k, v) in enumerate(seq2go.items()):
+
+        if verbose == 1:
+            sys.stdout.write("\r{0:.0f}%".format(100.0 * i / len(seq2go)))
+
+        if k not in seq2pssm:
+            continue
+        y = np.zeros(len(classes))
+        for go in onto.propagate(seq2go[k]):
+            if go not in s_cls:
+                continue
+            y[classes.index(go)] = 1
+
+        seq, pssm, msa = seq2pssm[k]
+        if not pssm: continue
+
+        x1 = [[AA.aa2onehot[aa] + [pssm[i][AA.index2aa[k]] for k in range(20)]] for i, aa in enumerate(seq)]
+        x2 = msa
+
+        yield k, x1, x2, y
+
+
+def batch_generator(gen, normalize=False, batch_size=8):
+    xByShapes, yByShapes = dict(), dict()
+    for _, x, _, y in gen:
+        x, y = np.array(x).reshape(1, len(x), 40), zeroone2oneminusone(y)
+        if normalize: x = np.divide(np.add(x, -np.mean(x)), np.std(x))
+        if x.shape in xByShapes:
+            xByShapes[x.shape].append(x)
+            yByShapes[x.shape].append(y)
+            X, Y = xByShapes[x.shape], yByShapes[x.shape]
+            if len(X) == batch_size:
+                yield np.array(X), np.array(Y)
+                del xByShapes[x.shape]
+                del yByShapes[x.shape]
+
+        else:
+            xByShapes[x.shape] = [x]  # initially a list, because we're going to append items
+            yByShapes[x.shape] = [y]
+
+    for X, Y in zip(xByShapes.values(), yByShapes.values()):
+        yield np.array(X), np.array(Y)
+
+
+def eval_generator(model, gen_xy, length_xy, classes):
+
+    pbar = tqdm(total=length_xy)
+    i, m, n = 0, length_xy, len(classes)
     y_pred, y_true = np.zeros((m, n)), np.zeros((m, n))
-    for x_shp, y_shp in zip(X.keys(), Y.keys()):
-        k = len(Y[y_shp])
-        y_hat, y = model.predict(X[x_shp]), Y[y_shp]
+    for i, (X, Y) in enumerate(gen_xy):
+        assert len(X) == len(Y)
+        k = len(Y)
+        y_hat, y = model.predict(X), Y
         y_pred[i:i+k, ], y_true[i:i+k, ] = y_hat, y
-        i += k
+
+        if (i + 1) % 20 == 0:
+            loss = np.mean(hinge(y, y_hat).eval(session=sess))
+            pbar.set_description("Validation Loss:%.5f" % loss)
+        pbar.update(k)
+
+    pbar.close()
+
+    y_pred = y_pred[~np.all(y_pred == 0, axis=1)]
+    y_true = y_true[~np.all(y_true == 0, axis=1)]
+
     loss = np.mean(hinge(y_true, y_pred).eval(session=sess))
     y_pred = oneminusone2zeroone(y_pred)
     y_true = oneminusone2zeroone(y_true)
-    f_max = F_max(y_pred, y_true, classes)
+    f_max = F_max(y_pred, y_true, classes, np.arange(0.1, 1, 0.1))
 
     return y_true, y_pred, loss, f_max
+
+
+def train_generator(model, gen_xy, length_xy, epoch, num_epochs, history=LossHistory(), lrate=LearningRateScheduler(step_decay)):
+
+    pbar = tqdm(total=length_xy)
+
+    for X, Y in gen_xy:
+        assert len(X) == len(Y)
+
+        model.fit(x=X, y=Y,
+                  batch_size=8, epochs=num_epochs,
+                  verbose=0,
+                  validation_data=None,
+                  initial_epoch=epoch,
+                  callbacks=[history, lrate])
+        pbar.set_description("Training Loss:%.5f" % np.mean(history.losses))
+        pbar.update(len(Y))
+
+    pbar.close()
 
 
 if __name__ == "__main__":
@@ -255,23 +286,19 @@ if __name__ == "__main__":
     print("Loading Ontology...")
     onto = get_ontology(ASPECT)
 
-    trn_seq2pssm, trn_seq2go, tst_seq2pssm, tst_seq2go = load_training_and_validation(db, limit=None)
-
-    train_and_validation_data = ({k: v[0] for k, v in trn_seq2pssm.items()}, trn_seq2go,
-                                 {k: v[0] for k, v in tst_seq2pssm.items()}, tst_seq2go)
-
     classes = onto.classes
-
-    trn_X, trn_Y = get_xy(data_generator(trn_seq2pssm, trn_seq2go, classes))
-    tst_X, tst_Y = get_xy(data_generator(tst_seq2pssm, tst_seq2go, classes))
 
     model = ModelCNN(classes)
     print(model.summary())
 
     sess = tf.Session()
     for epoch in range(args.num_epochs):
-        train(model, trn_X, trn_Y, epoch, args.num_epochs)
-        _, _, loss, f_max = evaluate(model, tst_X, tst_Y, classes)
+        trn_stream, tst_stream = get_training_and_validation_streams(db, classes)
+
+        train_generator(model, batch_generator(trn_stream), len(trn_stream), epoch, args.num_epochs)
+        _, _, loss, f_max = eval_generator(model, batch_generator(tst_stream), len(tst_stream), classes)
+
         print("[Epoch %d] (Validation Loss: %.5f, F_max: %.3f)" % (epoch + 1, loss, f_max))
+
         model_path = 'checkpoints/all-levels-cnn-%d-%.3f-%.2f.hdf5' % (epoch + 1, loss, f_max)
         model.save_weights(model_path)
