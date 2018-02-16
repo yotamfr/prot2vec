94426d4d53300432ae6db533a083a6e387322dd2
yotamfr <yotamfr@hotmail.com>
2018-02-16 10:04:11 +0200

    Returned CUDA_VISIBLE_DEVICES ...

--- src/python/benchmark-1.py
+++ src/python/benchmark-1.py
@@ -1,6 +1,6 @@
 import os
-# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-# os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 from src.python.consts import *
 
