e0037342c7e97c41b001ba1295ecc5907a09ac00
yotamfr <yotamfr@hotmail.com>
2018-02-17 11:19:03 +0200

    Commented out NVIDIA Env Vars.

--- src/python/benchmark-0.py
+++ src/python/benchmark-0.py
@@ -1,6 +1,6 @@
 import os
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 from src.python.consts import *
 
