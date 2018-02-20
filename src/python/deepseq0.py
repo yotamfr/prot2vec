c9425c7254edeefce294d8ce0f4406b0589ec78e
yotamfr <yotamfr@hotmail.com>
2018-02-20 10:47:53 +0200

    increased dropout to 0.5, doubled the network size.

--- src/python/deepseq0.py
+++ src/python/deepseq0.py
@@ -129,13 +129,13 @@ def Features(inpt):
 
     feats = Embedding(input_dim=26, output_dim=23, embeddings_initializer='uniform')(inpt)
 
-    feats = Conv1D(250, 15, activation='relu', padding='valid')(feats)
-    # feats = Dropout(0.3)(feats)
-    feats = Conv1D(100, 15, activation='relu', padding='valid')(feats)
-    # feats = Dropout(0.3)(feats)
-    feats = Conv1D(100, 15, activation='relu', padding='valid')(feats)
-    # feats = Dropout(0.3)(feats)
-    feats = Conv1D(250, 15, activation='relu', padding='valid')(feats)
+    feats = Conv1D(500, 15, activation='relu', padding='valid')(feats)
+    feats = Dropout(0.5)(feats)
+    feats = Conv1D(200, 15, activation='relu', padding='valid')(feats)
+    feats = Dropout(0.5)(feats)
+    feats = Conv1D(200, 15, activation='relu', padding='valid')(feats)
+    feats = Dropout(0.5)(feats)
+    feats = Conv1D(500, 15, activation='relu', padding='valid')(feats)
     feats = Dropout(0.5)(feats)
     feats = GlobalMaxPooling1D()(feats)
     return feats
