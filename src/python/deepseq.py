59a6b9c7f81727a8699cc02d7c73f03fe56143d9
yotamfr <yotamfr@hotmail.com>
2018-03-07 00:20:34 +0200

    deeperseq ... (5 layers - lowered dropout rate)

--- src/python/deepseq.py
+++ src/python/deepseq.py
@@ -130,15 +130,15 @@ def Features(inpt):
     feats = Embedding(input_dim=26, output_dim=30, embeddings_initializer='uniform')(inpt)
 
     feats = Conv1D(250, 30, activation='relu', padding='valid')(feats)
-    feats = Dropout(0.1)(feats)
+    feats = Dropout(0.2)(feats)
     feats = Conv1D(250, 10, activation='relu', padding='valid')(feats)
-    feats = Dropout(0.1)(feats)
+    feats = Dropout(0.2)(feats)
     feats = Conv1D(250, 10, activation='relu', padding='valid')(feats)
-    feats = Dropout(0.1)(feats)
+    feats = Dropout(0.2)(feats)
     feats = Conv1D(250, 10, activation='relu', padding='valid')(feats)
-    feats = Dropout(0.1)(feats)
+    feats = Dropout(0.2)(feats)
     feats = Conv1D(250, 10, activation='relu', padding='valid')(feats)
-    feats = Dropout(0.1)(feats)
+    feats = Dropout(0.2)(feats)
 
     return feats
 
