584c8ee289de8bb1ecd83bce44b3513e0bdd4585
yotamfr <yotamfr@hotmail.com>
2018-03-07 21:58:43 +0200

    deeperseq ... (added a "super" layer)

--- src/python/deepseq.py
+++ src/python/deepseq.py
@@ -153,13 +153,13 @@ def Features(inpt):
     feats = Embedding(input_dim=26, output_dim=23, embeddings_initializer='uniform')(inpt)
 
     feats = Dropout(0.3)(feats)
-    feats = Conv1D(750, 30, activation='relu', padding='valid')(feats)
+    feats = Conv1D(900, 30, activation='relu', padding='valid')(feats)
     feats = Dropout(0.3)(feats)
-    feats = Conv1D(250, 1, activation='linear', padding='valid')(feats)
+    feats = Conv1D(300, 1, activation='linear', padding='valid')(feats)
     feats = Dropout(0.3)(feats)
-    feats = Conv1D(100, 15, activation='relu', padding='valid')(feats)
+    feats = Conv1D(150, 15, activation='relu', padding='valid')(feats)
     feats = Dropout(0.3)(feats)
-    feats = Conv1D(250, 15, activation='relu', padding='valid')(feats)
+    feats = Conv1D(300, 15, activation='relu', padding='valid')(feats)
 
     return feats
 
